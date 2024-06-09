78fbfbc0fe242b84d1ff7ff274d3006e792c39d8
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-06-09 17:19:54 +0200

    chore: init project.

---
+++ Object.Mother.App/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Object.Mother.AutoFixture.App
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public static User Create(string name, int age)
+        {
+            return new User(name, age);
+        }
+    }
+}
---
+++ Object.Mother.Test/Fixtures/UserMother.cs
@@ -0,0 +1,27 @@
+﻿using Object.Mother.AutoFixture.App;
+
+namespace Object.Mother.AutoFixture.Test.Fixtures
+{
+    public class UserMother
+    {
+        public static User UserUnderEighteen()
+        {
+            return User.Create("Pepito Grillo", 16);
+        }
+
+        public static User UserOverEighteenUnderThirty()
+        {
+            return User.Create("Pepito Grillo", 29);
+        }
+
+        public static User UserOverThirtyUnderSixtyFive()
+        {
+            return User.Create("Pepito Grillo", 46);
+        }
+
+        public static User UserOverSixtyFive()
+        {
+            return User.Create("Pepito Grillo", 67);
+        }
+    }
+}
